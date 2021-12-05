049afbcff4993b660861b5c1ff54df9d2a7438fc
ni1985 <kir.nikel@gmail.com>
2021-12-05 21:31:24 +0100

    Possibility to make posts created

---
+++ network/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.forms import ModelForm
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    creator = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE)#,
+        #related_name='creator')
+    post_text = models.TextField()
+    post_time = models.DateTimeField(auto_now_add=True)
+    post_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Post id {self.id}; User: {self.creator}; Likes: {self.post_likes}"
+
+
+class Comments(models.Model):
+    user_id = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name='commenter')
+    post_id = models.ForeignKey(
+        Post,
+        on_delete = models.CASCADE)
+    date_time = models.DateTimeField(auto_now_add=True)
+    comment_text = models.TextField()
+
+    def __str__(self):
+        return f"Comment: {self.id}, User: {User.commenter}"
+
+
+class Follow(models.Model):
+    user_id = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name='follows')
+    subscribed = models.ManyToManyField(
+        User,
+        blank = True,
+        related_name='followed')
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['post_text']
---
+++ network/views.py
@@ -0,0 +1,86 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .models import User, Post, PostForm
+
+
+def index(request):
+    
+    # create a new post
+    if request.method == "POST":
+        new_post = PostForm(request.POST)
+
+        if new_post.is_valid():
+            post = new_post.save(commit=False)
+            post.creator = request.user
+            post.save()
+    
+        return HttpResponseRedirect(reverse('index'))
+
+    else:
+        # prepare a form for the new post
+        new_post_form = PostForm()
+
+        # retrieve all posts
+        posts = Post.objects.select_related('creator').order_by('-post_time')
+        print(posts)
+        print(posts.query)
+
+        return render(request, "network/index.html", {
+            'new_post':new_post_form,
+            'posts':posts
+        })
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
