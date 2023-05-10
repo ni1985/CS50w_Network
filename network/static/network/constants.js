9c82d89a3d2fe7edf0c57b9072e4b4bb739d8617
ni1985 <kir.nikel@gmail.com>
2023-05-10 23:53:18 +0200

    Project finished. Ready to be submitted to evaluation

--- network/static/network/constants.js
+++ network/static/network/constants.js
@@ -1,5 +1,5 @@
-const UPDATE_POST_LIKE_URL = 'update-post-like';
+const UPDATE_POST_LIKE_URL = '/update-post-like';
 export { UPDATE_POST_LIKE_URL };
 
-const UPDATE_POST_TEXT_URL = 'update-post-text';
+const UPDATE_POST_TEXT_URL = '/update-post-text';
 export { UPDATE_POST_TEXT_URL };
